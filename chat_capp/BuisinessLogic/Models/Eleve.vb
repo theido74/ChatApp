41ce29cedc612151942e523971b39bb54f067407
theido74 <arnaud.pnct@eduge.ch>
2026-04-27 09:01:14 +0200

    Fonction terminée GetEleveByUsername,GetEleveByID. Mise à jour Sprint2: Documentation SCRUM et livrables semaine 2

--- chat_capp/BuisinessLogic/Models/Eleve.vb
+++ chat_capp/BuisinessLogic/Models/Eleve.vb
@@ -3,16 +3,10 @@
 
     Public Property Niveau As String
     Public Property NbPoints As Integer
+    Public Property Classe As String
 
-    Public Sub New(niveau As String, nbPoints As Integer)
-        Me.Niveau = niveau
-        Me.NbPoints = nbPoints
-    End Sub
 
-    Public Sub AfficherDetails()
-        Console.WriteLine("Niveau: " & Niveau)
-        Console.WriteLine("Nombre de points: " & NbPoints)
-    End Sub
+
 
 End Class
 
