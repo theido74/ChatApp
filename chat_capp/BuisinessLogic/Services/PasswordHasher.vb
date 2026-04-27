10c4d3fd3c44afbe913b5cfc6683713eaf63f21c
theido74 <arnaud.pnct@eduge.ch>
2026-04-27 11:38:11 +0200

    FIN LUNDI SEMAINE1

--- chat_capp/BuisinessLogic/Services/PasswordHasher.vb
+++ chat_capp/BuisinessLogic/Services/PasswordHasher.vb
@@ -3,6 +3,30 @@ Imports System.Text
 
 Public Class PasswordHasher
     Private Const SALT_SIZE As Integer = 16
+    Private Const HASH_ITERATIONS As Integer = 1000
+
+    'Hache un mot de passe avec un salt aléatoire (PBKDF2-SHA256)
+    'Paramètre de la fonction = mot de passe en clair
+    'Retourne String (base64)
+
+    Public Shared Function HashMotdePasse(motDePasse As String) As String
+        'Génération d'un salt aléatoire
+        Dim saltBytes As Byte() = New Byte(SALT_SIZE - 1) {}
+        Using rng As New RNGCryptoServiceProvider()
+            rng.GetBytes(saltBytes)
+        End Using
+        ' Utiliser PBKDF2 (meilleur que simple SHA-256)
+        Using pbkdf2 As New Rfc2898DeriveBytes(motDePasse, saltBytes, HASH_ITERATIONS, HashAlgorithmName.SHA256)
+            Dim hashBytes As Byte() = pbkdf2.GetBytes(32) ' 32 bytes = 256 bits
+
+            ' Combiner salt + hash et encoder en base64
+            Dim combinedBytes As Byte() = New Byte(saltBytes.Length + hashBytes.Length - 1) {}
+            Buffer.BlockCopy(saltBytes, 0, combinedBytes, 0, saltBytes.Length)
+            Buffer.BlockCopy(hashBytes, 0, combinedBytes, saltBytes.Length, hashBytes.Length)
+
+            Return Convert.ToBase64String(combinedBytes)
+        End Using
+    End Function
 
 
 End Class
