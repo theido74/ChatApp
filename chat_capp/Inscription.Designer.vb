2c67aa83565b880911bfe8ec1e7d906c2327b40a
theido74 <arnaud.pnct@eduge.ch>
2026-05-04 14:52:12 +0200

    AddInscriptionForm

---
+++ chat_capp/Inscription.Designer.vb
@@ -0,0 +1,290 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Inscription
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Inscription))
+        Me.pnlInscription = New System.Windows.Forms.Panel()
+        Me.txtNiveau = New System.Windows.Forms.TextBox()
+        Me.lblNiveau = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.lblAge = New System.Windows.Forms.Label()
+        Me.btnAnnuler = New System.Windows.Forms.Button()
+        Me.btnInscription = New System.Windows.Forms.Button()
+        Me.txtConfirmMDP = New System.Windows.Forms.TextBox()
+        Me.txtMDP = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPrenom = New System.Windows.Forms.TextBox()
+        Me.txtNom = New System.Windows.Forms.TextBox()
+        Me.lblConfirmerMDP = New System.Windows.Forms.Label()
+        Me.lblMDP = New System.Windows.Forms.Label()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.lblPrenom = New System.Windows.Forms.Label()
+        Me.lblNom = New System.Windows.Forms.Label()
+        Me.lblSignup = New System.Windows.Forms.Label()
+        Me.pnlInscription.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlInscription
+        '
+        Me.pnlInscription.BackColor = System.Drawing.Color.DarkOrchid
+        Me.pnlInscription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlInscription.Controls.Add(Me.txtNiveau)
+        Me.pnlInscription.Controls.Add(Me.lblNiveau)
+        Me.pnlInscription.Controls.Add(Me.txtEmail)
+        Me.pnlInscription.Controls.Add(Me.Label1)
+        Me.pnlInscription.Controls.Add(Me.txtAge)
+        Me.pnlInscription.Controls.Add(Me.lblAge)
+        Me.pnlInscription.Controls.Add(Me.btnAnnuler)
+        Me.pnlInscription.Controls.Add(Me.btnInscription)
+        Me.pnlInscription.Controls.Add(Me.txtConfirmMDP)
+        Me.pnlInscription.Controls.Add(Me.txtMDP)
+        Me.pnlInscription.Controls.Add(Me.txtUsername)
+        Me.pnlInscription.Controls.Add(Me.txtPrenom)
+        Me.pnlInscription.Controls.Add(Me.txtNom)
+        Me.pnlInscription.Controls.Add(Me.lblConfirmerMDP)
+        Me.pnlInscription.Controls.Add(Me.lblMDP)
+        Me.pnlInscription.Controls.Add(Me.lblUsername)
+        Me.pnlInscription.Controls.Add(Me.lblPrenom)
+        Me.pnlInscription.Controls.Add(Me.lblNom)
+        Me.pnlInscription.Controls.Add(Me.lblSignup)
+        Me.pnlInscription.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.pnlInscription.Location = New System.Drawing.Point(246, 45)
+        Me.pnlInscription.Name = "pnlInscription"
+        Me.pnlInscription.Size = New System.Drawing.Size(308, 361)
+        Me.pnlInscription.TabIndex = 2
+        '
+        'txtNiveau
+        '
+        Me.txtNiveau.Location = New System.Drawing.Point(174, 192)
+        Me.txtNiveau.Name = "txtNiveau"
+        Me.txtNiveau.Size = New System.Drawing.Size(120, 20)
+        Me.txtNiveau.TabIndex = 20
+        '
+        'lblNiveau
+        '
+        Me.lblNiveau.AutoSize = True
+        Me.lblNiveau.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNiveau.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblNiveau.Location = New System.Drawing.Point(170, 169)
+        Me.lblNiveau.Name = "lblNiveau"
+        Me.lblNiveau.Size = New System.Drawing.Size(59, 20)
+        Me.lblNiveau.TabIndex = 19
+        Me.lblNiveau.Text = "Niveau :"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(174, 146)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(120, 20)
+        Me.txtEmail.TabIndex = 18
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(170, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 20)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Email :"
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(174, 100)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(120, 20)
+        Me.txtAge.TabIndex = 16
+        '
+        'lblAge
+        '
+        Me.lblAge.AutoSize = True
+        Me.lblAge.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAge.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblAge.Location = New System.Drawing.Point(170, 77)
+        Me.lblAge.Name = "lblAge"
+        Me.lblAge.Size = New System.Drawing.Size(41, 20)
+        Me.lblAge.TabIndex = 14
+        Me.lblAge.Text = "Age :"
+        '
+        'btnAnnuler
+        '
+        Me.btnAnnuler.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAnnuler.Location = New System.Drawing.Point(230, 335)
+        Me.btnAnnuler.Name = "btnAnnuler"
+        Me.btnAnnuler.Size = New System.Drawing.Size(75, 23)
+        Me.btnAnnuler.TabIndex = 13
+        Me.btnAnnuler.Text = "Annuler"
+        Me.btnAnnuler.UseVisualStyleBackColor = True
+        '
+        'btnInscription
+        '
+        Me.btnInscription.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInscription.Location = New System.Drawing.Point(3, 333)
+        Me.btnInscription.Name = "btnInscription"
+        Me.btnInscription.Size = New System.Drawing.Size(75, 23)
+        Me.btnInscription.TabIndex = 12
+        Me.btnInscription.Text = "S'inscrire"
+        Me.btnInscription.UseVisualStyleBackColor = True
+        '
+        'txtConfirmMDP
+        '
+        Me.txtConfirmMDP.Location = New System.Drawing.Point(16, 284)
+        Me.txtConfirmMDP.Name = "txtConfirmMDP"
+        Me.txtConfirmMDP.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtConfirmMDP.Size = New System.Drawing.Size(162, 20)
+        Me.txtConfirmMDP.TabIndex = 10
+        '
+        'txtMDP
+        '
+        Me.txtMDP.Location = New System.Drawing.Point(16, 238)
+        Me.txtMDP.Name = "txtMDP"
+        Me.txtMDP.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtMDP.Size = New System.Drawing.Size(162, 20)
+        Me.txtMDP.TabIndex = 9
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(16, 192)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(120, 20)
+        Me.txtUsername.TabIndex = 8
+        '
+        'txtPrenom
+        '
+        Me.txtPrenom.Location = New System.Drawing.Point(16, 146)
+        Me.txtPrenom.Name = "txtPrenom"
+        Me.txtPrenom.Size = New System.Drawing.Size(120, 20)
+        Me.txtPrenom.TabIndex = 7
+        '
+        'txtNom
+        '
+        Me.txtNom.Location = New System.Drawing.Point(16, 100)
+        Me.txtNom.Name = "txtNom"
+        Me.txtNom.Size = New System.Drawing.Size(120, 20)
+        Me.txtNom.TabIndex = 6
+        '
+        'lblConfirmerMDP
+        '
+        Me.lblConfirmerMDP.AutoSize = True
+        Me.lblConfirmerMDP.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmerMDP.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblConfirmerMDP.Location = New System.Drawing.Point(12, 261)
+        Me.lblConfirmerMDP.Name = "lblConfirmerMDP"
+        Me.lblConfirmerMDP.Size = New System.Drawing.Size(166, 20)
+        Me.lblConfirmerMDP.TabIndex = 5
+        Me.lblConfirmerMDP.Text = "Confirmer mot de passe :"
+        '
+        'lblMDP
+        '
+        Me.lblMDP.AutoSize = True
+        Me.lblMDP.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMDP.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblMDP.Location = New System.Drawing.Point(12, 215)
+        Me.lblMDP.Name = "lblMDP"
+        Me.lblMDP.Size = New System.Drawing.Size(99, 20)
+        Me.lblMDP.TabIndex = 4
+        Me.lblMDP.Text = "Mot de passe :"
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUsername.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblUsername.Location = New System.Drawing.Point(12, 169)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(79, 20)
+        Me.lblUsername.TabIndex = 3
+        Me.lblUsername.Text = "Username :"
+        '
+        'lblPrenom
+        '
+        Me.lblPrenom.AutoSize = True
+        Me.lblPrenom.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrenom.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblPrenom.Location = New System.Drawing.Point(12, 123)
+        Me.lblPrenom.Name = "lblPrenom"
+        Me.lblPrenom.Size = New System.Drawing.Size(70, 20)
+        Me.lblPrenom.TabIndex = 2
+        Me.lblPrenom.Text = "Prenom : "
+        '
+        'lblNom
+        '
+        Me.lblNom.AutoSize = True
+        Me.lblNom.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNom.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblNom.Location = New System.Drawing.Point(12, 77)
+        Me.lblNom.Name = "lblNom"
+        Me.lblNom.Size = New System.Drawing.Size(46, 20)
+        Me.lblNom.TabIndex = 1
+        Me.lblNom.Text = "Nom :"
+        '
+        'lblSignup
+        '
+        Me.lblSignup.AutoSize = True
+        Me.lblSignup.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSignup.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblSignup.Location = New System.Drawing.Point(107, 18)
+        Me.lblSignup.Name = "lblSignup"
+        Me.lblSignup.Size = New System.Drawing.Size(104, 25)
+        Me.lblSignup.TabIndex = 0
+        Me.lblSignup.Text = "Inscription"
+        '
+        'Inscription
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.pnlInscription)
+        Me.Name = "Inscription"
+        Me.Text = "Inscription"
+        Me.pnlInscription.ResumeLayout(False)
+        Me.pnlInscription.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlInscription As Panel
+    Friend WithEvents txtNiveau As TextBox
+    Friend WithEvents lblNiveau As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents lblAge As Label
+    Friend WithEvents btnAnnuler As Button
+    Friend WithEvents btnInscription As Button
+    Friend WithEvents txtConfirmMDP As TextBox
+    Friend WithEvents txtMDP As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPrenom As TextBox
+    Friend WithEvents txtNom As TextBox
+    Friend WithEvents lblConfirmerMDP As Label
+    Friend WithEvents lblMDP As Label
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents lblPrenom As Label
+    Friend WithEvents lblNom As Label
+    Friend WithEvents lblSignup As Label
+End Class
---
+++ chat_capp/Inscription.vb
@@ -0,0 +1,154 @@
+﻿Public Class Inscription
+
+
+    Private authService As New AuthenticationService()
+    Private clientValidator As New ClientValidator()
+    Private UserService As New UserService()
+    Private passwordHash As New PasswordHasher()
+
+    Public Class RegistrationData
+            Public Property Nom As String
+            Public Property Prenom As String
+            Public Property Username As String
+            Public Property Password As String
+            Public Property Age As Integer
+            Public Property Email As String
+            Public Property Niveau As String
+        End Class
+
+        ' Variable publique conservant les informations d'inscription après validation
+        Public PendingRegistration As RegistrationData = Nothing
+
+
+    ' Message d'erreur succinct
+    Private Sub AfficherErreur(msg As String)
+            MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Sub
+
+        ' Message de succès succinct
+        Private Sub AfficherSucces(msg As String)
+            MessageBox.Show(msg, "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Sub
+
+
+    Private Sub btnInscription_Click(sender As Object, e As EventArgs) Handles btnInscription.Click
+            ' Récupérer valeurs
+            Dim nom = txtNom.Text.Trim()
+            Dim prenom = txtPrenom.Text.Trim()
+            Dim username = txtUsername.Text.Trim()
+            Dim password = txtMDP.Text
+        Dim confirmPassword = txtConfirmMDP.Text.Trim()
+
+        Dim ageValue As Integer = 0
+            Dim email = txtEmail.Text.Trim()
+            Dim niveau = txtNiveau.Text.Trim()
+
+            ' Vérifications basiques de présence
+            If String.IsNullOrEmpty(nom) OrElse String.IsNullOrEmpty(prenom) Then
+                AfficherErreur("Le nom et le prénom sont requis.")
+                Return
+            End If
+
+            If String.IsNullOrEmpty(username) Then
+                AfficherErreur("Le nom d'utilisateur est requis.")
+                Return
+            End If
+
+            If String.IsNullOrEmpty(password) Then
+                AfficherErreur("Le mot de passe est requis.")
+                Return
+            End If
+
+        ' Vérifier âge (si présent)
+        If Not String.IsNullOrWhiteSpace(txtAge.Text) Then
+            If Not Integer.TryParse(txtAge.Text.Trim(), ageValue) Then
+                AfficherErreur("L'âge doit être un nombre entier.")
+                Return
+            End If
+            If ageValue < 5 OrElse ageValue > 120 Then
+                AfficherErreur("L'âge semble invalide.")
+                Return
+            End If
+        End If
+
+        ' Vérifier niveau (si présent)
+        If String.IsNullOrWhiteSpace(niveau) Then
+                AfficherErreur("Le niveau est requis.")
+                Return
+            End If
+
+            ' Vérifier que les services sont disponibles (runtime)
+            If clientValidator Is Nothing OrElse authService Is Nothing Then
+                AfficherErreur("Erreur interne : services non initialisés (mode designer ?).")
+                Return
+            End If
+
+            ' Réutiliser les règles du login pour username et password
+            If Not clientValidator.ValidateUsername(username) Then
+                AfficherErreur("Nom d'utilisateur invalide (respecter les règles).")
+                Return
+            End If
+
+            If Not clientValidator.ValidatePassword(password) Then
+                AfficherErreur("Mot de passe invalide (respecter les règles).")
+                Return
+            End If
+
+            ' Vérifier confirmation du mot de passe
+            If Not password.Equals(confirmPassword) Then
+                AfficherErreur("Les mots de passe ne correspondent pas.")
+                Return
+            End If
+
+            ' Vérifier unicité du username via le service existant
+            Try
+                Dim existingUser = authService.GetEleveByUsername(username)
+                If existingUser IsNot Nothing Then
+                    AfficherErreur("Nom d'utilisateur déjà utilisé. Choisissez-en un autre.")
+                    Return
+                End If
+            Catch ex As Exception
+                AfficherErreur("Impossible de vérifier l'unicité du nom d'utilisateur : " & ex.Message)
+                Return
+            End Try
+
+        ' Tous les contrôles passés : stocker les données dans PendingRegistration
+        PendingRegistration = New RegistrationData With {
+                .Nom = nom,
+                .Prenom = prenom,
+                .Username = username,
+                .Password = password,
+                .Age = ageValue,
+                .Email = email,
+                .Niveau = niveau
+            }
+
+        UserService.CreateEleve(username, nom, prenom, dateDeNaissance, email, passwordHash.HashMotdePasse(password), niveau, 0, classe)
+
+        AfficherSucces("Inscription prête. Les données sont stockées en mémoire pour insertion en base.")
+        ' Option : appeler directement le service d'enregistrement ici si disponible,
+        ' sinon rediriger vers le Login et insérer depuis là en récupérant PendingRegistration.
+        Dim loginform As New Login()
+        loginform.ShowDialog()
+            Me.Close()
+        End Sub
+
+        Private Sub btnAnnuler_Click(sender As Object, e As EventArgs) Handles btnAnnuler.Click
+            Me.Close()
+        End Sub
+
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Inscription
+        '
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Name = "Inscription"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+
+
+
+End Class
