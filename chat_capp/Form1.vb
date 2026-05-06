38ffcfaa98125c87e837c7cad6c7efd335bf209e
damienRSR <160753181+damienRSR@users.noreply.github.com>
2026-05-06 14:01:59 +0200

    Form1V1.1

--- chat_capp/Form1.vb
+++ chat_capp/Form1.vb
@@ -3,9 +3,37 @@
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Tester la connexion au démarrage
-        TestOracleConnection()
+        ' TestOracleConnection()
+        GetMessages()
+    End Sub
 
+    Private Sub GetForums()
+        Dim forumDataAccess = New ForumDataAccess
+        Dim forums As List(Of Forum) = forumDataAccess.GetAllForums
+        For Each forum As Forum In forums
+            Console.WriteLine("ID: " & forum.ForumId)
+            Console.WriteLine("Nom: " & forum.NomForum)
+            Console.WriteLine("Description: ")
+            Console.WriteLine("DateCreation: ")
+            Console.WriteLine("Supprimé: ")
+            Console.WriteLine("-----------------------------")
+        Next
+    End Sub
 
+    Private Sub GetMessages()
+        Dim messageDataAccess As New messageDataAccess()
+        Dim userMessages As List(Of Message) = messageDataAccess.GetMessageByRecipientId(2)
+        For Each msg As Message In userMessages
+            Console.WriteLine("ID: " & msg.MessageId)
+            Console.WriteLine("Expéditeur: " & msg.EmmeteurId)
+            Console.WriteLine("Destinataire: " & msg.ReceveurId)
+            Console.WriteLine("Forum: " & msg.ForumId)
+            Console.WriteLine("Contenu: " & msg.Contenu)
+            Console.WriteLine("Date: " & msg.TimeStamp)
+            Console.WriteLine("Privé: " & msg.EstPrive)
+            Console.WriteLine("Supprimé: " & msg.EstSupprime)
+            Console.WriteLine("-----------------------------")
+        Next
     End Sub
 
     Private Sub TestOracleConnection()
