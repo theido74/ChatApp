73b9efa73d405264fb7ba6069cd861f54a565e00
theido74 <arnaud.pnct@eduge.ch>
2026-05-06 14:50:44 +0200

    fix case filename

--- chat_capp/DataAccess/MessageDataAccess.vb
+++ chat_capp/DataAccess/MessageDataAccess.vb
@@ -1,11 +1,9 @@
-﻿'Imports System.Data
-'Imports System.Windows
-'Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+﻿Imports System.Data
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
 Imports Oracle.ManagedDataAccess.Client
 
 Public Class messageDataAccess
-
-    Public Function CreateMessage(idEnvoyeur As Integer, idReceveur As Integer, contenu As String, Optional forum As Integer? = Nothing) As Integer
+    Public Function CreateMessage(idEnvoyeur As Integer, idReceveur As Integer, contenu As String) As Integer
         Dim newId As Integer = 0
 
         Try
@@ -23,13 +21,7 @@ Public Class messageDataAccess
 
                         cmd.Parameters.Add("idEnvoyeur", OracleDbType.Int32).Value = idEnvoyeur
                         cmd.Parameters.Add("idReceveur", OracleDbType.Int32).Value = idReceveur
-
-                        If forum.HasValue Then
-                            cmd.Parameters.Add("forumId", OracleDbType.Int32).Value = forum.Value
-                        Else
-                            cmd.Parameters.Add("forumId", OracleDbType.Int32).Value = DBNull.Value
-                        End If
-
+                        cmd.Parameters.Add("forumId", OracleDbType.Int32).Value = 22
                         cmd.Parameters.Add("contenu", OracleDbType.Varchar2).Value = contenu
                         cmd.Parameters.Add("timeStamp", OracleDbType.Date).Value = DateTime.Now
                         cmd.Parameters.Add("estPrive", OracleDbType.Int16).Value = 0
@@ -43,66 +35,14 @@ Public Class messageDataAccess
                         newId = Convert.ToInt32(prmNewId.Value.ToString())
 
                         tx.Commit()
-
                         Return newId
                     End Using
                     tx.Rollback()
                 End Using
-
             End Using
-
         Catch ex As Exception
             MessageBox.Show("Erreur BD: " & ex.Message)
-
             Return -1
-
         End Try
-    End Function
-
-    Public Function GetMessageByRecipientId(id As Integer) As List(Of Message)
-        Dim messages As New List(Of Message)()
-
-        Try
-            Using conn As OracleConnection = DatabaseConnection.GetConnection() ' Using -> à la fin la variable "conn" se ferme; GetConnection() -> retourne une connexion oracle configurée.
-                conn.Open() ' Rend la connexion active.
-
-                Dim sql As String = "SELECT mes_id, mes_per_id_emm, mes_per_id_rec, mes_for_id, mes_contenu, mes_timestamp, mes_estprive, mes_estsupprime " &
-                    "FROM ess_message " &
-                    "WHERE mes_estsupprime = 0 AND mes_per_id_rec = :id " &
-                    "ORDER BY mes_for_id ASC, mes_per_id_emm ASC, mes_timestamp DESC"
-
-                Using cmd As New OracleCommand(sql, conn)
-                    cmd.Parameters.Add("id", OracleDbType.Int32).Value = id
-                    cmd.CommandType = CommandType.Text
-                    cmd.CommandTimeout = 30
-
-                    Using reader As OracleDataReader = cmd.ExecuteReader()
-                        While reader.Read()
-                            Dim forumId As Integer = -1
-                            If Not IsDBNull(reader("mes_for_id")) Then
-                                forumId = CInt(reader("mes_for_id"))
-                            End If
-
-                            Dim message As New Message With {
-                                .MessageId = CInt(reader("mes_id")),
-                                .EmmeteurId = CInt(reader("mes_per_id_emm")),
-                                .ReceveurId = CInt(reader("mes_per_id_rec")),
-                                .ForumId = forumId,
-                                .Contenu = reader("mes_contenu").ToString(),
-                                .TimeStamp = CDate(reader("mes_timestamp")),
-                                .EstPrive = CBool(reader("mes_estprive")),
-                                .EstSupprime = CBool(reader("mes_estsupprime"))
-                            }
-                            messages.Add(message)
-                        End While
-                    End Using
-                End Using
-            End Using
-            Return messages
-
-        Catch ex As Exception
-            MessageBox.Show("Erreur BD: " & ex.Message)
-        End Try
-        Return Nothing
     End Function
 End Class
